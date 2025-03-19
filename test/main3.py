7ce70f470e2095cc68c892d5b076878376d3f8ee
quocdungms <quocdungms@gmail.com>
2025-03-19 16:22:29 +0700

    relocate decode-location_mode

--- test/main3.py
+++ test/main3.py
@@ -1,6 +1,8 @@
 import asyncio
 import time
-import struct
+from queue import Queue
+from xmlrpc.client import Boolean
+
 import pytz
 import socketio
 from bleak import BleakClient, BleakScanner, BleakError
@@ -23,67 +25,6 @@ command_queue = asyncio.Queue()
 
 MODULE_STATUS = {}
 TASK_MANAGER = {}
-
-
-def decode_location_data(data):
-    try:
-        mode = data[0]
-        if mode == 0:
-            if len(data) <= 13:
-                print("Invalid Type 0 data: Expected 13 bytes")
-                return None
-            return decode_location_mode_0(data)
-        elif mode == 1:
-            return decode_location_mode_1(data)
-        elif mode == 2:
-            return decode_location_mode_2(data)
-        else:
-            print(f"Unknown location mode: {mode}")
-
-    except Exception as e:
-        print(f"Error decoding location: {e}")
-        return None
-
-
-# Position Only
-def decode_location_mode_0(data):
-    result = {}
-    # location_mode = data[0]
-    # result["Mode:"] = location_mode
-    x, y, z, quality_position = struct.unpack("<i i i B", data[1:14])
-    result["Position"] = {
-        "X": x / 1000,  # Chuyển từ mm sang m
-        "Y": y / 1000,
-        "Z": z / 1000,
-        "Quality Factor": quality_position
-    }
-    return result
-
-# Distances Only
-def decode_location_mode_1(data):
-    result = {}
-    distances = []
-    distance_count = data[0]
-    result["Distances count:"] = distance_count
-    for i in range(distance_count):
-        offset = 1 + i * 7
-        node_id, distance, quality = struct.unpack("<H i B", data[offset:offset + 7])
-        distances.append({
-            "Node ID": node_id,
-            "Distance": distance / 1000,  # Chuyển từ mm sang m
-            "Quality Factor": quality
-        })
-    result["Distances"] = distances
-    return result
-
-# Hàm giải mã Location Data Mode 2 (Position + Distances)
-def decode_location_mode_2(data):
-    result = {}
-    mode_0 = decode_location_mode_0(data[:14])
-    mode_1 = decode_location_mode_1(data[14:])
-    result.update(mode_0)
-    result.update(mode_1)
-    return result
 async def safe_emit(event, data):
     if sio.connected:
         await sio.emit(event, data)
